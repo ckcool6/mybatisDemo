b5a85150c4ffeaf7bbaded2b6bbe67ba123bb65e
rzdtjb <rzdtjb@gmail.com>
2023-05-25 21:25:58 +0800

    proxy

--- src/com/it/service/impl/StudentServiceImpl.java
+++ src/com/it/service/impl/StudentServiceImpl.java
@@ -2,38 +2,182 @@ package com.it.service.impl;
 
 import com.it.bean.Student;
 import com.it.mapper.StudentMapper;
-import com.it.mapper.impl.StudentMapperImpl;
 import com.it.service.StudentService;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class StudentServiceImpl implements StudentService {
 
     //创建持久层对象
-    private StudentMapper mapper = new StudentMapperImpl();
+
 
     @Override
     public List<Student> selectAll() {
-        return mapper.selectAll();
+        List<Student> list = null;
+        SqlSession sqlSession = null;
+        InputStream is = null;
+
+        try {
+            is = Resources.getResourceAsStream("MyBatisConfig.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+            sqlSession = sqlSessionFactory.openSession(true);
+
+            //
+            StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+            list = mapper.selectAll();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return list;
+
     }
 
     @Override
     public Student selectById(Integer id) {
-        return mapper.selectById(id);
+        Student stu = null;
+        SqlSession sqlSession = null;
+        InputStream is = null;
+
+        try {
+            is = Resources.getResourceAsStream("MyBatisConfig.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+            sqlSession = sqlSessionFactory.openSession(true);
+
+            //
+            StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+            stu = mapper.selectById(id);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return stu;
     }
 
     @Override
     public Integer insert(Student stu) {
-        return mapper.insert(stu);
+        Integer result = null;
+        SqlSession sqlSession = null;
+        InputStream is = null;
+
+        try {
+            is = Resources.getResourceAsStream("MyBatisConfig.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+            sqlSession = sqlSessionFactory.openSession(true);
+
+            //
+            StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+            result = mapper.insert(stu);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return result;
+
     }
 
     @Override
     public Integer update(Student stu) {
-        return mapper.update(stu);
+        Integer result = null;
+        SqlSession sqlSession = null;
+        InputStream is = null;
+
+        try {
+            is = Resources.getResourceAsStream("MyBatisConfig.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+            sqlSession = sqlSessionFactory.openSession(true);
+
+            //
+            StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+            result = mapper.update(stu);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return result;
+
     }
 
     @Override
     public Integer delete(Integer id) {
-        return mapper.delete(id);
+        Integer result = null;
+        SqlSession sqlSession = null;
+        InputStream is = null;
+
+        try {
+            is = Resources.getResourceAsStream("MyBatisConfig.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+            sqlSession = sqlSessionFactory.openSession(true);
+
+            //
+            StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+            result = mapper.delete(id);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return result;
+
     }
 }
