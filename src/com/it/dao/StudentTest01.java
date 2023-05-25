5e460490efee657f6e2e586a61019aa99d6b7f9d
rzdtjb <rzdtjb@gmail.com>
2023-05-25 17:10:26 +0800

    crud

--- src/com/it/dao/StudentTest01.java
+++ src/com/it/dao/StudentTest01.java
@@ -34,4 +34,62 @@ public class StudentTest01 {
         sqlSession.close();
         is.close();
     }
+
+    @Test
+    public void selectById() throws IOException {
+        InputStream is = Resources.getResourceAsStream("MyBatisConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Student stu = sqlSession.selectOne("StudentMapper.selectById", 3);
+        System.out.println(stu);
+        sqlSession.close();
+        is.close();
+    }
+
+    @Test
+    public void insert() throws IOException {
+        InputStream is = Resources.getResourceAsStream("MyBatisConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        Student stu = new Student(4, "赵六", 26);
+        int result = sqlSession.insert("StudentMapper.insert", stu);
+        sqlSession.commit();
+
+        System.out.println(result);
+
+        sqlSession.close();
+        is.close();
+    }
+
+    @Test
+    public void update() throws IOException {
+        InputStream is = Resources.getResourceAsStream("MyBatisConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        Student stu = new Student(5, "周七", 21);
+        int result = sqlSession.update("StudentMapper.update", stu);
+        sqlSession.commit();
+
+        System.out.println(result);
+
+        sqlSession.close();
+        is.close();
+    }
+
+    @Test
+    public void delete() throws IOException {
+        InputStream is = Resources.getResourceAsStream("MyBatisConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        int result = sqlSession.delete("StudentMapper.delete", 5);
+        sqlSession.commit();
+
+        System.out.println(result);
+
+        sqlSession.close();
+        is.close();
+    }
 }
